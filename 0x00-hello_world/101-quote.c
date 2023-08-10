ce99d91e13a506a5853a1414e2886a97538955fc
Abdo <tarekabdo1fg@gmail.com>
2023-08-10 21:41:40 +0300

    nothing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Entry point
  *
  * Description: A C program that prints with write function
  *
- * Return: Always 1 (Success)
+ * Return: Always 1 (not Success)
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59)
+	char abdo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1,abdo,59);
 	return (1);
 }
